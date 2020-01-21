7a93372a832388433d787bca47ba716759697ea9
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2020-01-21 23:25:02 +0300

    change attack

--- src/js/allowedValues.js
+++ src/js/allowedValues.js
@@ -33,22 +33,56 @@ export function allowedValues(position, distance, boardSize) {
 }
 
 export function allowedValuesAttack(position, distance, boardSize) {
+  // атака в пределах квадрата
+  // const allowValues = [];
+  // const itemRow = Math.floor(position / boardSize);
+  // const itemColumn = position % boardSize;
+  // let columnStart = 0;
+  // let rowStart = 0;
+  // let columnEnd = 7;
+  // let rowEnd = 7;
+
+  // columnStart = (itemColumn - distance) > 0 ? itemColumn - distance : 0;
+  // columnEnd = (itemColumn + distance) < 7 ? itemColumn + distance : 7;
+  // rowStart = (itemRow - distance) > 0 ? itemRow - distance : 0;
+  // rowEnd = (itemRow + distance) < 7 ? itemRow + distance : 7;
+
+  // for (let i = columnStart; i <= columnEnd; i += 1) {
+  //   for (let j = rowStart; j <= rowEnd; j += 1) {
+  //     allowValues.push((j * 8) + i);
+  //   }
+  // }
+  // return allowValues;
+
+  // атака по прямой и диагонали
   const allowValues = [];
   const itemRow = Math.floor(position / boardSize);
   const itemColumn = position % boardSize;
-  let columnStart = 0;
-  let rowStart = 0;
-  let columnEnd = 7;
-  let rowEnd = 7;
 
-  columnStart = (itemColumn - distance) > 0 ? itemColumn - distance : 0;
-  columnEnd = (itemColumn + distance) < 7 ? itemColumn + distance : 7;
-  rowStart = (itemRow - distance) > 0 ? itemRow - distance : 0;
-  rowEnd = (itemRow + distance) < 7 ? itemRow + distance : 7;
-
-  for (let i = columnStart; i <= columnEnd; i += 1) {
-    for (let j = rowStart; j <= rowEnd; j += 1) {
-      allowValues.push((j * 8) + i);
+  for (let i = 1; i <= distance; i += 1) {
+    if ((itemColumn + i) < 8) {
+      allowValues.push((itemRow * 8) + (itemColumn + i));
+    }
+    if ((itemColumn - i) >= 0) {
+      allowValues.push((itemRow * 8) + (itemColumn - i));
+    }
+    if ((itemRow + i) < 8) {
+      allowValues.push(((itemRow + i) * 8) + itemColumn);
+    }
+    if ((itemRow - i) >= 0) {
+      allowValues.push(((itemRow - i) * 8) + itemColumn);
+    }
+    if ((itemRow + i) < 8 && (itemColumn + i) < 8) {
+      allowValues.push(((itemRow + i) * 8) + (itemColumn + i));
+    }
+    if ((itemRow - i) >= 0 && (itemColumn - i) >= 0) {
+      allowValues.push(((itemRow - i) * 8) + (itemColumn - i));
+    }
+    if ((itemRow + i) < 8 && (itemColumn - i) >= 0) {
+      allowValues.push(((itemRow + i) * 8) + (itemColumn - i));
+    }
+    if ((itemRow - i) >= 0 && (itemColumn + i) < 8) {
+      allowValues.push(((itemRow - i) * 8) + (itemColumn + i));
     }
   }
   return allowValues;
